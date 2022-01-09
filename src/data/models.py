8592d7adc57c23d7d9b232acbbc9b31a1775d99d
Gennaro Coppola <coppola612@gmail.com>
2022-01-09 13:04:15 +0200

    First draft feature-1 "add models"

---
+++ src/data/models.py
@@ -0,0 +1,56 @@
+from sqlalchemy import Integer, Column, String, Boolean, Date, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+from datetime import datetime
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+    user_id = Column(Integer, primary_key=True)
+    createdOn = Column(DateTime)
+    username = Column(String(50))
+    email = Column(String(256))
+    password = Column(String)
+    cases = relationship("Case", back_populates="createdBy")
+    evidences = relationship("Evidence", back_populates="createdBy")
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.createdOn = datetime.now()
+
+    def __repr__(self) -> str:
+        return f"User(id={self.user_id!r}, username={self.username!r}, email={self.email!r})"
+
+
+class Case(Base):
+    __tablename__ = "case"
+    case_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    createdBy = relationship("User", back_populates="cases")
+    name = Column(String(50))
+    description = Column(String(2000))
+    deleted = Column(Boolean)
+    deletedOn = Column(DateTime)
+    updatedOn = Column(DateTime)
+    createdOn = Column(DateTime)
+    evidences = relationship("Evidence", back_populates="case")
+
+    def __repr__(self) -> str:
+        return f"Case(id={self.case_id!r}"
+
+
+class Evidence(Base):
+    __tablename__ = "evidence"
+    evidence_id = Column(Integer, primary_key=True)
+    case_id = Column(Integer, ForeignKey('case.case_id'))
+    case = relationship("Case", back_populates="evidences")
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    createdBy = relationship("User", back_populates="evidences")
+    createdOn = Column(DateTime)
+    content = Column(String)
+
+    def __repr__(self) -> str:
+        return f"Evidence(id={self.evidence_id!r}) belongs to {self.case!r})"
---
+++ test/test_data.py
@@ -0,0 +1,15 @@
+def test_database_creation():
+    import data
+    result = data.session.query(data.User).all()
+    assert len(result) == 0
+
+
+def test_insert():
+    import data
+    data.session.begin()
+    data.session.add(data.User("test_username", "email", "password1"))
+    data.session.commit()
+
+    result = data.session.query(data.User).all()
+    print(f"Fetched user is {result[0]!r}")
+    assert len(result) == 1
