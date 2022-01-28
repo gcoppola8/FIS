4fd2d315dec7162a3dd6bbcf21df37ae7665d053
Gennaro Coppola <coppola612@gmail.com>
2022-01-28 18:59:42 +0200

    added database fake data

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,19 +1,28 @@
 import os
 import uuid
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import create_session
 
-sqlite = "db.sqlite"
+db_name = "db.sqlite"
 
 if "PYTEST_RUN_CONFIG" in os.environ:
-    sqlite = str(uuid.uuid1()) + '.sqlite'
+    db_name = str(uuid.uuid1()) + '.sqlite'
 
-print(f"database name: {sqlite}")
+print(f"database name: {db_name}")
 
-engine = create_engine("sqlite+pysqlite:///%s" % sqlite, echo=True)
+engine = create_engine("sqlite+pysqlite:///%s" % db_name, echo=True)
 session = create_session(engine)
 
 from data.models import *
 
 Base.metadata.create_all(engine)
+
+if session.query(User).get(1) is None:
+    import sqlite3
+    connection = sqlite3.connect(db_name)
+
+    with open("script.sql", 'r') as s:
+        sql_script = s.read()
+        connection.executescript(sql_script)
+    s.closed
